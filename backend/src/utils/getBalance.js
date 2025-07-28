dff0b9f3a89b7381c1d1c5517e77fc846e455fd0
DD <3181244967@qq.com>
2025-07-28 22:22:37 +0800

    后端初步完成项目管理后功能

--- backend/src/utils/getBalance.js
+++
@@ -1,9 +0,0 @@
-import Web3 from 'web3';
-
-export const getBalance = async (address) => {
-    const web3 = new Web3('http://192.168.139.129:8545');
-    const balance = await web3.eth.getBalance(address);
-    console.log(balance);
-    return balance;
-}
-
